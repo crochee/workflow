e1b819984958721f25d505c4b343f9f471d9efed
crochee <13522570308@139.com>
2022-01-29 22:34:15 +0800

    [Feature] add ut

--- stack_test.go
+++ stack_test.go
@@ -9,18 +9,19 @@ import (
 
 func TestStack(t *testing.T) {
 	s := NewStack()
-	assert.Equal(t, 0, s.length)
+	assert.Equal(t, 0, s.Length())
 	p1 := NewPipelineFlow()
 	name1 := p1.Name()
-	s.Push(NewVertex(p1))
-	assert.Equal(t, 1, s.length)
-
+	v1 := NewVertex(p1)
+	s.Push(v1)
+	assert.Equal(t, 1, s.Length())
+	assert.Equal(t, v1, s.Top())
 	p2 := NewSpawnFlow()
 	name2 := p2.Name()
 	v := NewVertex(p2)
 	fmt.Println(v)
 	s.Push(v)
-	assert.Equal(t, 2, s.length)
+	assert.Equal(t, 2, s.Length())
 
 	assert.Equal(t, name2, s.Pop().Flow.Name())
 	assert.Equal(t, name1, s.Pop().Flow.Name())
